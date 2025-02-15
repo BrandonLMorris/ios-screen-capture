e0b1255234abb4ad6be312057d81c05074d9d62e
Brandon Morris <github@brandonmorris.dev>
2025-02-15 09:22:37 -0800

    initial refactor of audio clock packet handling

--- Sources/ScreenCapture/Recorder.swift
+++ Sources/ScreenCapture/Recorder.swift
@@ -17,14 +17,12 @@ public class Recorder {
   private let output: MediaReceiver = AVAssetReceiver(to: "/tmp/recording.mp4")!
   private let verbose: Bool
 
-  private var audioStartTime: Time! = nil
   private var deviceAudioStart: Time! = nil
   private var localAudioLatest: Time! = nil
   private var deviceAudioLatest: Time! = nil
 
   private var videoRequest: VideoDataRequest! = nil
   private let closeStreamGroup = DispatchGroup()
-  private var audioClockRef: CFTypeID = 0
 
   public init(verbose: Bool = false) {
     self.verbose = verbose
@@ -56,7 +54,7 @@ public class Recorder {
       return
     }
     logger.info("Beginning recorder shutdown...")
-    try device.send(packet: CloseStream(clock: audioClockRef))
+    try device.send(packet: CloseStream(clock: context.audioClockRef))
     try device.send(packet: CloseStream())
     let closeResult = closeStreamGroup.wait(wallTimeout: .now() + .seconds(3))
     switch closeResult {
@@ -93,11 +91,11 @@ public class Recorder {
     logger.trace("Handling \(type(of: packet)) packet", metadata: ["desc": "\(packet)"])
     switch packet {
     case let p as Ping:
-      try p.onReceive(context)
+      try p.onReceive(&context)
     case let controlPacket as ControlPacket:
-      try controlPacket.onReceive(context)
+      try controlPacket.onReceive(&context)
     case let audioClockPacket as AudioClock:
-      try handle(audioClockPacket)
+      try audioClockPacket.onReceive(&context)
     case let audioFormatPacket as AudioFormat:
       let audioFormatReply = audioFormatPacket.reply()
       logger.debug(
@@ -124,38 +122,6 @@ public class Recorder {
       logger.warning(
         "Unexpected packet received!", metadata: ["base64": "\(packet.data.base64EncodedString())"])
     }
-  }
-
-  // MARK: Control (go/stop) packet handling
-
-  private func handle(_ controlPacket: ControlPacket) throws {
-    if controlPacket.header.subtype == PacketSubtype.goRequest {
-      let goReply = controlPacket.reply()
-      logger.debug("Sending go reply", metadata: ["desc": "\(goReply.description)"])
-      try device.send(packet: goReply)
-    }
-    if controlPacket.header.subtype == PacketSubtype.stopRequest {
-      let stopReply = controlPacket.reply()
-      logger.debug("Sending stop reply", metadata: ["desc": "\(stopReply.description)"])
-      try device.send(packet: stopReply)
-    }
-  }
-
-  // MARK: Audio clock (cwpa) packet handling
-
-  private func handle(_ audioClockPacket: AudioClock) throws {
-    let desc = HostDescription()
-    logger.debug("Sending host description packet", metadata: ["desc": "\(desc.description)"])
-    try device.send(packet: desc)
-    logger.debug("Sending stream desc")
-    audioClockRef = audioClockPacket.clock.clock
-    try device.send(packet: StreamDescription(clock: audioClockRef))
-    self.audioStartTime = Time.now()
-    let audioClockReply = Reply(
-      correlationId: audioClockPacket.clock.correlationId,
-      clock: audioClockPacket.clock.clock + 1000)
-    logger.debug("Sending audio clock reply", metadata: ["desc": "\(audioClockReply.description)"])
-    try device.send(packet: audioClockReply)
   }
 
   // MARK: Video clock (cvrp) packet handling
@@ -211,7 +177,7 @@ public class Recorder {
       self.output.sendVideo(mediaSample.sample)
       try device.send(packet: self.videoRequest)
     case .audio:
-      self.localAudioLatest = Time.now().since(self.audioStartTime)
+      self.localAudioLatest = Time.now().since(context.audioStartTime)
       self.deviceAudioLatest = mediaSample.sample.outputPresentation ?? Time.NULL
       if deviceAudioStart == nil {
         self.deviceAudioStart = deviceAudioLatest
@@ -221,12 +187,15 @@ public class Recorder {
 }
 
 protocol RecordingPacket {
-  func onReceive(_ context: RecordingContext) throws
+  func onReceive(_ context: inout RecordingContext) throws
 }
 
 struct RecordingContext {
   private let device: CaptureStream
   private let mediaReceiver: MediaReceiver
+
+  public var audioClockRef: CFTypeID = 0
+  public var audioStartTime: Time = Time.NULL!
 
   internal init(_ device: CaptureStream, _ mediaReceiver: MediaReceiver) {
     self.device = device
--- Sources/ScreenCapture/ScreenCapturePacket+RecordingPacket.swift
+++ Sources/ScreenCapture/ScreenCapturePacket+RecordingPacket.swift
@@ -1,20 +1,39 @@
 import Foundation
 import Logging
+import Object
 import Packet
 
 private let logger = Logger(label: "ScreenCapturePacket+RecordingPacket")
 
 extension Ping: RecordingPacket {
-  func onReceive(_ context: RecordingContext) throws {
+  func onReceive(_ context: inout RecordingContext) throws {
     try context.send(packet: Ping.instance)
   }
 }
 
 extension ControlPacket: RecordingPacket {
-  func onReceive(_ context: RecordingContext) throws {
+  func onReceive(_ context: inout RecordingContext) throws {
     let subtype = header.subtype == .goRequest ? "go" : "stop"
     let replyPacket = reply()
     logger.debug("Sending \(subtype) reply", metadata: ["desc": "\(replyPacket.description)"])
     try context.send(packet: replyPacket)
+  }
+}
+
+extension AudioClock: RecordingPacket {
+  func onReceive(_ context: inout RecordingContext) throws {
+    let desc = HostDescription()
+    logger.debug("Sending host description packet", metadata: ["desc": "\(desc.description)"])
+    try context.send(packet: desc)
+    logger.debug("Sending stream desc")
+    context.audioClockRef = clock.clock
+    try context.send(packet: StreamDescription(clock: context.audioClockRef))
+    context.audioStartTime = Time.now()
+    let audioClockReply = Reply(
+      correlationId: clock.correlationId,
+      clock: clock.clock + 1000)
+    logger.debug("Sending audio clock reply", metadata: ["desc": "\(audioClockReply.description)"])
+    try context.send(packet: audioClockReply)
+
   }
 }
